63da74dbf4640a43a046c670f46a274d9f1f3d26
= <=>
2022-05-20 10:15:48 +0700

    update

--- models/user.js
+++ models/user.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
@@ -17,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     name: DataTypes.STRING,
-    role: DataTypes.STRING
+    status: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'user',
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,29 @@
 // Create db variable and get database connection here ...
 // Get QueryTypes from sequelize
+const { append } = require("express/lib/response")
+const db = require("../database/connection")
 
 // Create controller add User here ...
+exports.addUser = async (req, res) => {
+    try {
+        const { email, password, name, status } = req.body
+
+        const query = `INSERT INTO users(email,password,name,status) VALUES ('${email}','${password}','${name}','${status}')`
+
+        await db.sequelize.query(query)
+
+        res.send({
+            status: "Success",
+            message: "Add user finished"
+        })
+
+    } catch (error) {
+        res.send({
+            status: "Failed",
+            message: "Server error"
+        })
+    }
+}
 
 // Create controller get Users here ...
 
